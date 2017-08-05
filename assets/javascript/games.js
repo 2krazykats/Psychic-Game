de08d9fd31675d2e3c8e87b77706713759d77855
Elizabeth Nguyen <2krazykats@gmail.com>
2017-08-05 15:50:19 +0100

    almost working

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,33 +1,93 @@
-/* This is the script file for the Animal Hangman Game */
-// Declare the variable for the array of animals
-var animals = ["llama","elephant","rhinoceros","kangaroo","porpoise"];
+/* This is the script file for the Psychic Game */
 
-function selectAnimal (animals) {
 
-    // We need to randomize which value in the array is used
-    var getAnimalWord = animals[Math.floor(Math.random() * animals.length)];
-    // var hangmanWord = array[getAnimalWord];
-    return getAnimalWord;
+// VARIABLES
+// ==========================================================================
+var chosenLetter = "";
+var maxGuess = 10;
+var guessCount = 10;
+var lossCount = 0;
+var winsCount = 0;
+
+
+// FUNCTIONS
+// ==========================================================================
+// Randomly choose the letter to use
+function chooseLetter() {
+    var charSet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (var i=0; i < charSet.length; i++ ) {
+        chosenLetter += charSet.charAt(Math.floor(Math.random() * charSet.length));
+console.log(chosenLetter);
+        return chosenLetter;
+    }
 }
 
-document.getElementById("currentWord").innerHTML = selectAnimal(getAnimalWord);
+function resetForNewGame() {
+    guessCount = 10;
+    usedLettersCount = 0;
+    usedLetters = "";
 
-// Declare the variable for the letters already used
-var lettersAlreadyUsed = [];
+    document.getElementById("winCount").innerHTML = `${winsCount} !`;
+    document.getElementById("lossCount").innerHTML = `${lossCount} !`;
+    document.getElementById("guessLeftCount").innerHTML = guessCount;
+    document.getElementById("lettersAlreadyUsed").innerHTML = "";
+}
+
+function updateWins() {
+    document.getElementById("winCount").innerHTML = `${winsCount} !`;
+}
+
+function updateLoses() {
+    document.getElementById("lossCount").innerHTML = `${lossCount} !`;
+}
+
+function updateGuesses() {
+    document.getElementById("guessLeftCount").innerHTML = guessCount;
+}
+
+function newGame() {
+        chooseLetter();
+        resetForNewGame();
+        updateWins();
+        updateLoses();
+        return;
+}
+
+// MAIN CODE
+// ==========================================================================
+// Print the random letter to the console
+console.log(chooseLetter());
 
 document.onkeyup = function(event) {
-    //Declare a var for each letter entered
-    var userGuess = event.key;
-    document.getElementById("lettersAlreadyUsed").insertAdjacentHTML('beforeend', `${userGuess}, `);
+    var inputLetter = String.fromCharCode(event.keyCode).toLowerCase();
 
-}
+    // Get the letters the users entered and write them to the
+    // id=lettersAlreadyUsed
+    var usedLetters = document.getElementById("lettersAlreadyUsed").innerHTML += `${inputLetter}, `;
+    var usedLettersCount= usedLetters.replace(/[^A-Z]/gi, "").length;
+console.log(`This is usedLettersCount: ${usedLettersCount}`);
 
-// Creates blanks for each letter in the word
-function blanksWord (getAnimalWord) {
-    var result = "" ;
+//    Guesses left should be decremented each time thru
+    if (usedLettersCount < maxGuess) {
+        guessCount--;
+        console.log(`This is guessCount: ${guessCount}`);
+        updateGuesses();
+    }
 
+// If usedLettersCount is > maxGuess then alert loss and increment lossCount
+    if (usedLettersCount >= maxGuess) {
+        alert("You lost!");
 
+        lossCount++;
+        newGame();
+    }
 
+// If the user chooses the right letter, alert them and increment the winsCount
+    if (chosenLetter === inputLetter) {
+        alert("Letter matches!");
 
-
+        winsCount++;
+        newGame();
+    }
 }
